cd88ba386c5bffeb7f36ed83206574bea0db7237
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-19 10:04:30 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+   <User />
     </div>
   );
 }
+const User = () =>{
+  return (
+    <div>
+      <h1>Name: Nasim</h1>
+      <h1>Age: 69</h1>
+      <h1>Email: qureshi.nasim@yahoo.com</h1>
+    </div>
+  );
+};
 
 export default App;
